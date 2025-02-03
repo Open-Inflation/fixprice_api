b16fb58993f3cfe86f05e8e49b853d1d8fa4001d
Miskler <lysenkomiha913@gmail.com>
2025-02-03 07:50:03 +0300

    base logic

--- fixprice_api/api.py
+++ fixprice_api/api.py
@@ -1,15 +1,34 @@
 import aiohttp
+from io import BytesIO
+
 
 class FixPriceAPI:
     def __init__(self):
-        self.session = aiohttp.ClientSession()
+        self._session = aiohttp.ClientSession()
 
     async def __aenter__(self):
         return self
 
     async def __aexit__(self, *exc):
-        await self.session.close()
+        await self._session.close()
     
-    async def fetch(self, path: str) -> dict:
-        async with self.session.get(f"{path}") as response:
+    async def fetch(self, url: str, method: str = "GET", **kwargs) -> dict:
+        # TODO Возвращать так же поля x-city (id города), x-count (количество товаров удовлетворяющих запросу), x-launguage (язык ответа)
+
+        async with self._session.request(method, url, **kwargs) as response:
+            if response.status != 200:
+                raise ValueError(f"Failed to fetch data, status code: {response.status}")
             return await response.json()
+
+    async def download_image(self, url: str) -> BytesIO:
+        if not self._session:
+            await self.__aenter__()
+        
+        async with self._session.get(url) as response:
+            if response.status == 200:
+                image = BytesIO(await response.read())
+                image.name = f"{url.split('/')[-1]}"
+
+                return image
+            else:
+                raise ValueError(f"Failed to download image, status code: {response.status}")
--- fixprice_api/manager.py
+++ fixprice_api/manager.py
@@ -1,10 +1,10 @@
-import asyncio
-from api import FixPriceAPI
-from pprint import pprint
+from .api import FixPriceAPI
+from io import BytesIO
+from .abstraction import CatalogSort
 
 
 class FixPrice:
-    BASE_URL = "https://api.fix-price.com"
+    BASE_URL = "https://api.fix-price.com/buyer/v1"
     def __init__(self):
         self.api = FixPriceAPI()
 
@@ -14,14 +14,26 @@ class FixPrice:
     async def __aexit__(self, *exc):
         await self.api.__aexit__(*exc)
     
-    async def catalog(self):
-        return await self.api.fetch(f"{self.BASE_URL}/buyer/v1/category/menu")
+    async def categories_list(self) -> dict:
+        return await self.api.fetch(f"{self.BASE_URL}/category/menu")
+    
+    async def products_list(
+            self,
+            category_alias: str,
+            subcategory_alias: str = None,
+            page: int = 1,
+            limit: int = 20,
+            sort: CatalogSort = CatalogSort.POPULARITY
+        ) -> dict:
+        url = f"{self.BASE_URL}/product/in/{category_alias}"
+        if subcategory_alias: url += f"/{subcategory_alias}"
+        url += f"?page={page}&limit={limit}&sort={sort}"
+        
+        print(url)
+        return await self.api.fetch(url=url, method="POST")
+    
+    async def download_image(self, url: str) -> BytesIO:
+        """Скачать изображение с сайта."""
+        return await self.api.download_image(url)
 
-
-
-async def main():
-    async with FixPrice() as fixprice:
-        pprint(await fixprice.catalog())
-
-if __name__ == "__main__":
-    asyncio.run(main())
+    # TODO функция выбора города (поиск id городов по названию), информация о городе по id, страница товара, проверка наличия товара в точках города, получение информации о точках, получение списка городов, получение списка стран
