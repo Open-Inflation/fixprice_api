711f05cec45496eb6f358aefcfdd90d5819da7cd
Miskler <lysenkomiha913@gmail.com>
2025-02-07 21:38:57 +0300

    complite

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fixprice_api',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'aiohttp'
@@ -11,7 +11,7 @@ setup(
     description='A Python API client for FixPrice catalog',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/Open-Inflation/perekrestok_api',
+    url='https://github.com/Open-Inflation/fixprice_api',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
